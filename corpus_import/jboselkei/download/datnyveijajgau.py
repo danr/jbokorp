be5add1e06c11cf2e11af88dc5dd198df77a5c1d
Dan Rosén <danr@chalmers.se>
2014-05-07 23:14:45 +0200

    jboselkei complete

--- corpus_import/jboselkei/download/datnyveijajgau.py
+++ corpus_import/jboselkei/download/datnyveijajgau.py
@@ -1,6 +1,11 @@
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
+import lxml.etree as ET
 import HTMLParser
+import re
+
+amp = re.compile(r"\\&")
 h = HTMLParser.HTMLParser()
+
 # print h.unescape('&pound;682m')
 
 def table_to_dict(tree):
@@ -8,7 +13,18 @@ def table_to_dict(tree):
     for tbl in tree.iter("table"):
         d = {}
         for clmn in tbl.iter("column"):
-            d[clmn.attrib['name']] = h.unescape(clmn.text)
+            t = clmn.text
+            if t:
+                o = "" + t
+                t = amp.sub("&",t)
+                t = h.unescape(h.unescape(t))
+                # if o != t:
+                #     print "####"
+                #     print o
+                #     print "===="
+                #     print t
+                #     print "^^^^"
+            d[clmn.attrib['name']] = t
         ret[d['id']] = d
     return ret
 
@@ -19,6 +35,28 @@ users = file_to_dict('users.xml')
 trans = file_to_dict('translations.xml')
 posts = file_to_dict('posts.xml')
 
+msgs = ET.Element('text')
+
 for i,d in trans.items():
-    print d['entry']
-    print users[d['authorid']]['name']
+    # print "########"
+    # print i
+    # print d['entry']
+    if d['entry']:
+        msg = ET.SubElement(msgs, 'msg', 
+            id=i,
+            author=users[d['authorid']]['name'],
+            difficulty=posts[d['postid']]['difficulty'],
+            sourceauthor=users[posts[d['postid']]['authorid']]['name'],
+            sourceid=d['postid'],
+            source=posts[d['postid']]['entry'] or '',
+            sourcelang=posts[d['postid']]['language'],
+            sourcecomment=posts[d['postid']]['comment'] or '',
+            comment=d['comment'] or '',
+            date=d['date'].split(" ")[0],
+            time=d['date'].split(" ")[1],
+            value=d['value'],
+            reviews=d['reviews'])
+        msg.text = d['entry']
+
+print ET.tostring(msgs, pretty_print = True)
+
